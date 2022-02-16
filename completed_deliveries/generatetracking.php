0402daf987d427ddcd38a8679233915249572c3c
yovin60 <paranavithanayovin@gmail.com>
2022-02-17 01:59:35 +0530

    Update generatetracking.php

--- completed_deliveries/generatetracking.php
+++ completed_deliveries/generatetracking.php
@@ -11,7 +11,15 @@ $cname = $_SESSION['cname'];
         $status = 'PICKED';
 
         $db = mysqli_connect('localhost', 'root', '', 'pickandgo');
-        $sql = "UPDATE tracking SET status='DELIVERED', location='{$cname}' WHERE picked_id='{$id}'";
+        $result = mysqli_query($db, "SELECT * FROM pickedup_items INNER JOIN pickup_orders ON pickedup_items.order_id = pickup_orders.order_id WHERE pickedup_items.picked_id='{$id}'");
+
+        while($row = mysqli_fetch_array($result))
+                {
+                  $location= $row['receiver_address'];
+                  
+                }
+
+        $sql = "UPDATE tracking SET status='DELIVERED', location='{$location}' WHERE picked_id='{$id}'";
   
         if (mysqli_query($db,$sql)) {
             
